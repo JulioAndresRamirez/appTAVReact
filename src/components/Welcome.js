006d0a3e87e4d806314bb218be54d7f9d48ebac5
Julio Andres Ramirez De Freitas <julio157ramirez@gmail.com>
2020-07-10 12:14:19 -0400

    App ET preliminar

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,47 +1,47 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import "./Styles/Welcome.css";
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './Styles/Welcome.css';
 // Assets
-import Logo from "../components/assets/Logo_app/logo_tav.png";
-import ThanksImages from "../components/assets/Logo_app/logo_escuela_duoc.png";
+import Logo from '../components/assets/Logo_app/logoET.png';
+import Girls from '../components/assets/imageGirls.png';
+import ThanksImages from '../components/assets/Logo_app/logo_escuela_duoc.png';
 
 function welcome() {
-    return (
-        <div className="Welcome">
-            <div className="Content">
-                <div className="Logo">
-                    <img src={Logo} alt="Logo" />
-                    <p className="descs">
-                        Consulta información relevante sobre tus pruebas finales
-                        de periodo TAV
-                    </p>
-                </div>
-                <div className="Thanks">
-                    <img
-                        style={{
-                            width: 150
-                        }}
-                        src={ThanksImages}
-                        alt="imagen_agradecimiento"
-                    />
-                    <p
-                        style={{
-                            opacity: 0.6,
-                            color: "#282828"
-                        }}
-                        className="Thanks--captions"
-                    >
-                        Este proyecto fue realizado por alumnos de Duoc UC Sede
-                        Melipilla en colaboración con el Programa de Ingles.
-                    </p>
-
-                    <Link className="button" to="/GiveInfo">
-                        Continuar
-                    </Link>
-                </div>
-            </div>
+  return (
+    <div className="Welcome">
+      <div className="Content">
+        <div className="Logo">
+          <img src={Logo} alt="Logo" />
+          <p className="descs">
+            Consulta información relevante acerca de tus Examenes Transversales.
+          </p>
         </div>
-    );
+        <div className="Thanks">
+          <img
+            style={{
+              width: 150,
+            }}
+            src={ThanksImages}
+            alt="imagen_agradecimiento"
+          />
+          <p
+            style={{
+              opacity: 0.6,
+              color: '#282828',
+            }}
+            className="Thanks--captions"
+          >
+            Este proyecto fue realizado por alumnos de Duoc UC Sede Melipilla en
+            colaboración con el Programa de Ingles.
+          </p>
+
+          <Link className="button" to="/GiveInfo">
+            Continuar
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default welcome;
