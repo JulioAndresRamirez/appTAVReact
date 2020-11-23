7f551be039b32848934558ae07c33fc4060f4c67
Julio Andres Ramirez De Freitas <julio157ramirez@gmail.com>
2020-11-22 21:21:01 -0300

    Mejoras en el mensaje al no encontrar examanes

--- src/components/Error.js
+++ src/components/Error.js
@@ -1,46 +1,46 @@
-import React from "react";
+import React from 'react';
 // CSS
-import "./Styles/Error.css";
+import './Styles/Error.css';
 // IMg
-import Error_img from "./assets/Error_img/error_notFound.png";
+import Error_img from './assets/Error_img/error_notFound.png';
 
 export default function Error(props) {
-    return (
-        <div
-            style={{
-                display: "flex",
-                flexDirection: "column",
-                justifyContent: "center",
-                alignItems: "center"
-            }}
-            className="Error"
-        >
-            <div
-                style={{
-                    marginTop: "-50px"
-                }}
-            >
-                <img src={Error_img} alt="Not Found" />
+  return (
+    <div
+      style={{
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}
+      className="Error"
+    >
+      <div
+        style={{
+          marginTop: '-50px',
+        }}
+      >
+        <img src={Error_img} alt="Not Found" />
 
-                <h1
-                    style={{
-                        color: "black",
-                        fontSize: "24px",
-                        marginBottom: "10px"
-                    }}
-                >
-                    Aparenteme no te encuentras en nuestra base de datos.
-                </h1>
-                <p
-                    style={{
-                        color: "black",
-                        fontSize: "14px",
-                        opacity: 0.7
-                    }}
-                >
-                    Contacta a tu coordinador e informate sobre tus test.
-                </p>
-            </div>
-        </div>
-    );
+        <h1
+          style={{
+            color: 'black',
+            fontSize: '24px',
+            marginBottom: '10px',
+          }}
+        >
+          No se encontraron Examanes Transversales relacionados a ti.
+        </h1>
+        <p
+          style={{
+            color: 'black',
+            fontSize: '14px',
+            opacity: 0.7,
+          }}
+        >
+          Contacta a tu coordinador para mas información.
+        </p>
+      </div>
+    </div>
+  );
 }
